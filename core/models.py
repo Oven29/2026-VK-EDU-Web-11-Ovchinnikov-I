2109c49a2bcebd74b946de98891f2c9134ce82ee
Oven29 <ovchinno.ivan@gmail.com>
2026-05-12 04:06:34 +0300

    feat: create all models
    -

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from .managers import ProfileManager
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
+    photo = models.ImageField(
+        upload_to='avatars/%Y/%m/%d/',
+        default='avatars/default.png',
+        blank=True,
+        null=True,
+        verbose_name='Аватар'
+    )
+
+    objects = ProfileManager()
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from .managers import QuestionManager, TagManager
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63, unique=True,
+                            db_index=True, verbose_name='Название')
+
+    objects = TagManager()
+
+    def __str__(self):
+        return f'Тег {self.name}'
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='questions', verbose_name='Автор')
+    tags = models.ManyToManyField(
+        Tag, blank=True, related_name='questions', verbose_name='Теги')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата создания')
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return f'Вопрос {self.title}'
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Answer(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='answers', verbose_name='Автор')
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='answers', verbose_name='Вопрос')
+    is_correct = models.BooleanField(
+        default=False, verbose_name='Правильный ответ')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'Ответ на "{self.question.title}" от {self.author.username}'
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+
+class LikeAbstract(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+    VOTE_CHOICES = (
+        (LIKE, 'Лайк'),
+        (DISLIKE, 'Дизлайк'),
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    value = models.SmallIntegerField(
+        choices=VOTE_CHOICES,
+        default=LIKE,
+        verbose_name='Значение'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class QuestionLike(LikeAbstract):
+    question = models.ForeignKey(
+        'Question',
+        on_delete=models.CASCADE,
+        related_name='likes',
+        verbose_name='Лайк вопроса'
+    )
+
+    class Meta(LikeAbstract.Meta):
+        unique_together = ('user', 'question')
+        verbose_name = 'Лайк вопроса'
+        verbose_name_plural = 'Лайки вопросов'
+
+
+class AnswerLike(LikeAbstract):
+    answer = models.ForeignKey(
+        'Answer',
+        on_delete=models.CASCADE,
+        related_name='likes',
+        verbose_name='Лайк ответа'
+    )
+
+    class Meta(LikeAbstract.Meta):
+        unique_together = ('user', 'answer')
+        verbose_name = 'Лайк ответа'
+        verbose_name_plural = 'Лайки ответов'
