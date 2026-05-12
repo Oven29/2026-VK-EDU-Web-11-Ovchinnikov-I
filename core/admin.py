c13ff20bbb04f649e878530ea25c5b3c240d0415
Oven29 <ovchinno.ivan@gmail.com>
2026-05-12 04:15:53 +0300

    feat: setup admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Профиль'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'photo')
+    search_fields = ('user__username', 'user__email')
+    raw_id_fields = ('user',)
--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Question, Answer, QuestionLike, AnswerLike
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+    raw_id_fields = ('author',)
+    fields = ('author', 'text', 'is_correct', 'rating', 'created_at')
+    readonly_fields = ('created_at',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'rating', 'created_at')
+    list_filter = ('created_at', 'tags')
+    search_fields = ('title', 'text')
+    raw_id_fields = ('author',)
+    filter_horizontal = ('tags',)
+    inlines = [AnswerInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('author').prefetch_related('tags')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'author', 'is_correct', 'rating', 'created_at')
+    list_filter = ('is_correct', 'created_at')
+    search_fields = ('text',)
+    raw_id_fields = ('author', 'question')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('author', 'question')
+
+
+@admin.register(QuestionLike)
+class QuestionLikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'value')
+    list_filter = ('value',)
+    raw_id_fields = ('user', 'question')
+
+
+@admin.register(AnswerLike)
+class AnswerLikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'answer', 'value')
+    list_filter = ('value',)
+    raw_id_fields = ('user', 'answer')
